73fa2634599f512801a4e724e0459c11dd49b52f
Nico Clack <nico@bn7.net>
2017-05-30 18:12:33 +0100

    Bug fixes.

--- LittleLib.js
+++ LittleLib.js
@@ -112,8 +112,25 @@ function post(url, data, code) {
 	//Send the proper header information along with the request
 	http.setRequestHeader("Content-type", "application/json")
 	
-	http.onreadystatechange = code
+	http.onreadystatechange = function() {
+		if (http.readyState == 4) {
+			code()
+		}
+	}
 	http.send(params)
+}
+
+function get(url, func) {
+	// From https://stackoverflow.com/questions/9713058/send-post-data-using-xmlhttprequest and https://stackoverflow.com/questions/18962799/javascript-http-post-with-json-data
+    http = new XMLHttpRequest()
+	http.open("GET", url, true)
+	code = func
+	http.onreadystatechange = function() { 
+		if (http.readyState == 4) {
+			code()
+		}
+	}
+	http.send()
 }
 
 try {
